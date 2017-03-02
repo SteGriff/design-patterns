f6a6f2df8be86338c0e7bd56a4acbc1be66c1ac6
Dominic Bisset <dominicbisset@gmail.com>
2017-03-02 13:33:44 +0000

    Added comment to make note of future simplification

--- factory-method/typescript/src/Utilities/TypeOf.ts
+++ factory-method/typescript/src/Utilities/TypeOf.ts
@@ -1,5 +1,8 @@
 ///Typescript's built-in typeof can only distinguish between primitive types so returns [Object]
-///Borrowed from https://www.stevefenton.co.uk/2013/04/obtaining-a-class-name-at-runtime-in-typescript/
+//Borrowed from https://www.stevefenton.co.uk/2013/04/obtaining-a-class-name-at-runtime-in-typescript/
+//When compiling for es6 it should be possible to use
+//      thing.constructor.name
+//though will need to think about how minification affects naming.
 export function TypeOf(thing){
     var funcNameRegex = /function (.{1,})\(/;
     var results = (funcNameRegex).exec((<any> thing).constructor.toString());
