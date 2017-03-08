0f987cd6acb2c5ca036245f3ee995263efd5c20a
Ste Griffiths <github@stegriff.co.uk>
2017-03-08 08:34:34 +0000

    Start on adapter

---
+++ adapter/code/Adapter/JsonFileReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class JsonFileReader : IDataFileReader
+    {
+        string jsonText =
+@"[
+	{Name : 'Rolf', Age : 20},
+	{Name : 'Dabe', Age : 42},
+	{Name : 'Lex', Age : 33}
+]";
+
+        public object ReadNextRecord()
+        {
+            
+        }
+
+        public object ReadWholeFile()
+        {
+
+        }
+    }
+}
